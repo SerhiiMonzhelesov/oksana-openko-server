9b37ca94c11d7cec9abd4b83adceae86b4c00b98
SerhiiMonzhelesov <monzhelesov@gmail.com>
2023-10-29 17:46:51 +0200

    cors

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ const corsOptions = {
   optionsSuccessStatus: 204,
   // "200 // some legacy browsers (IE11, various SmartTVs) choke on 204",
   credentials: true,
-  headersAllowed: ["Access-Control-Allow-Origin", "http://localhost:3000"],
 };
 
 app.use(logger(formatsLogger));
